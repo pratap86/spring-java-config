69492dfb28bfc8c0f07a0dcb1bb29d1325b42547
Pratap Narayan <narayanpratap86@gmail.com>
2019-11-05 17:30:41 +0530

    First Commit

---
+++ src/main/java/com/pratap/spring/springjavaconfig/Dao.java
@@ -0,0 +1,8 @@
+package com.pratap.spring.springjavaconfig;
+
+public class Dao {
+
+	public void create() {
+		System.out.println("Created");
+	}
+}
---
+++ src/main/java/com/pratap/spring/springjavaconfig/Test.java
@@ -0,0 +1,14 @@
+package com.pratap.spring.springjavaconfig;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class Test {
+
+	public static void main(String[] args) {
+		AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(SpringConfig.class);
+		Service service = context.getBean(Service.class);
+		service.save();
+		context.close();
+	}
+
+}
